1b1f86f8c63cb25ba43255612fb5362de5fb6a9b
onayna <rkayaeomaima@gmail.com>
2023-08-22 16:27:26 -0700

    conv i and d

--- _writechar.c
+++ _writechar.c
@@ -1,42 +1,88 @@
-#include "main.h"
+#include <stdarg.h>
 #include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- * Description: _putchar uses a local buffer of 1024 to call write
- * as little as possible
+ *
+ * Return: On success 1, on error -1.
  */
 int _putchar(char c)
 {
-	static char buf[1024];
-	static int i;
-
-	if (c == -1 || i >= 1024)
-	{
-		write(1, &buf, i);
-		i = 0;
-	}
-	if (c != -1)
-	{
-		buf[i] = c;
-		i++;
-	}
-	return (1);
+    return write(1, &c, 1);
 }
 
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
- * Return: number of chars written
+ * _printf - simplified printf function
+ * @format: format string
+ * @...: variadic arguments
+ *
+ * Return: number of characters printed
  */
-int _puts(char *str)
+int _printf(const char *format, ...)
 {
-	register int i;
+    int printed = 0;
+    va_list args;
 
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-	return (i);
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            _putchar(*format);
+            printed++;
+        }
+        else
+        {
+            format++; // Move past '%'
+            if (*format == 'd' || *format == 'i')
+            {
+                int num = va_arg(args, int);
+                int num_copy = num;
+                int digits = 0;
+
+                if (num < 0)
+                {
+                    _putchar('-');
+                    printed++;
+                    num_copy = -num_copy;
+                }
+
+                while (num_copy > 0)
+                {
+                    num_copy /= 10;
+                    digits++;
+                }
+
+                while (digits > 0)
+                {
+                    int divisor = 1;
+                    int i = digits - 1;
+
+                    while (i > 0)
+                    {
+                        divisor *= 10;
+                        i--;
+                    }
+
+                    _putchar((num / divisor) % 10 + '0');
+                    printed++;
+                    digits--;
+                }
+            }
+        }
+        format++;
+    }
+
+    va_end(args);
+
+    return printed;
+}
+
+int main(void)
+{
+    _printf("This is an integer: %d\n", 42);
+    _printf("Another integer: %i\n", -123);
+    return 0;
 }
