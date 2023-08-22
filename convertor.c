99f27c339c6c39ac873de6a1a69864f39a5c219f
onayna <rkayaeomaima@gmail.com>
2023-08-22 16:44:24 -0700

    conv b

--- convertor.c
+++ convertor.c
@@ -1,27 +1,69 @@
-#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * convert - converts number and base into string
- * @num: input number
- * @base: input base
- * @lowercase: flag if hexa values need to be lowercase
- * Return: result string
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1, on error -1.
  */
-char *convert(unsigned long int num, int base, int lowercase)
+int _putchar(char c)
 {
-	static char *rep;
-	static char buffer[50];
-	char *ptr;
+    return write(1, &c, 1);
+}
 
-	rep = (lowercase)
-		? "0123456789abcdef"
-		: "0123456789ABCDEF";
-	ptr = &buffer[49];
-	*ptr = '\0';
-	do {
-		*--ptr = rep[num % base];
-		num /= base;
-	} while (num != 0);
+/**
+ * print_binary - prints an unsigned integer in binary format
+ * @n: The unsigned integer to print
+ */
+void print_binary(unsigned int n)
+{
+    if (n > 1)
+        print_binary(n / 2);
+    _putchar((n % 2) + '0');
+}
 
-	return (ptr);
+/**
+ * _printf - simplified printf function
+ * @format: format string
+ * @...: variadic arguments
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    int printed = 0;
+    va_list args;
+
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            _putchar(*format);
+            printed++;
+        }
+        else
+        {
+            format++; // Move past '%'
+            if (*format == 'b')
+            {
+                unsigned int num = va_arg(args, unsigned int);
+                print_binary(num);
+                printed += 32; // Assuming 32-bit unsigned int
+            }
+        }
+        format++;
+    }
+
+    va_end(args);
+
+    return printed;
+}
+
+int main(void)
+{
+    _printf("Binary representation of %b: %b\n", 42, 255);
+    return 0;
 }
